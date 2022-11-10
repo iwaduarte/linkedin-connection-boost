d1da15adaa14927882f302984c6d0b66387885ca
Iwá Duarte <iwaduarte@gmail.com>
2022-11-09 22:40:03 -0300

    feat: boost connections

--- puppeteer/boost.js
+++ puppeteer/boost.js
@@ -6,6 +6,42 @@ const SEARCH_INPUT = "";
 const SEARCH_BUTTON = "";
 const { USERNAME, PASSWORD } = process.env;
 
+const connect = () => {
+  let i = 0;
+  let count = 0;
+  const getAllConnections = () =>
+    Array.from(
+      document.querySelectorAll('[id*="ember"] > span.artdeco-button__text')
+    ).filter((e) => e.innerText === "Connect");
+
+  let allConnections = getAllConnections();
+
+  const interval = setInterval(async () => {
+    scroll(0, document.body.clientHeight);
+    if (i + 1 > allConnections.length) {
+      document.querySelector('[aria-label="Next"]')?.click();
+      await new Promise((r) => setTimeout(r, 500));
+      allConnections = getAllConnections();
+      i = 0;
+      return;
+    }
+
+    console.log("allConnections", allConnections[i]);
+    allConnections[i].click();
+    await new Promise((r) => setTimeout(r, 300));
+    const sendInvite = document.querySelector('[aria-label="Send now"]');
+    if (sendInvite.disabled)
+      document.querySelector('[aria-label="Dismiss"]').click();
+    else sendInvite?.click();
+
+    i++;
+    count++;
+    console.log("Connect: ", count);
+  }, 1400);
+
+  const stopOperation = () => clearInterval(interval);
+};
+
 const naiveBoost = async (page) => {
   await page.goto(LINKEDIN_URL);
   //login USERNAME | PASSWORD
