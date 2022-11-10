d76452d4e188dd59738190d1050579c59efd36df
Iwá Duarte <iwaduarte@gmail.com>
2022-11-09 22:44:56 -0300

    feat: run multiple queries through google search

--- puppeteer/email.js
+++ puppeteer/email.js
@@ -1,67 +1,147 @@
 import { extractEmailsAndNames, getEmail } from "../controllers/email.js";
-import { hashData } from "../utils.js";
+import { delay, hashData } from "../utils.js";
 import { cachedData } from "../cachedData.js";
 
 //query
 //?q=site%3Alinkedin.com+%22software+engineer%22+%40gmail.com+United+States
 
-const SEARCH_INPUT_CLASS = ".gLFyf";
-const SEARCH_BUTTON_CLASS = ".FPdoLc.lJ9FBc  input.gNO89b";
 const PAGE_RESULT_CLASS = ".v7W49e";
 const NEXT_PAGE_ID = "#pnnext";
 
-// const GOOGLE_URL = "https://www.google.com/search";
-const GOOGLE_URL = "http://localhost:3000/search";
-const SEARCH_INPUT = `site:linkedin.com "software engineer" @gmail.com United States`;
+// const GOOGLE_URL = "http://localhost:3000/search";
+
+const professions = [
+  '"software engineer"',
+  '"developer"',
+  '"full stack"',
+  '"recruiter"',
+  '"CTO"',
+  '"CEO"',
+];
+const emails = ['"@gmail.com"'];
+const countries = [
+  "United States",
+  "Canada",
+  "Brasil",
+  "United Kingdom",
+  "Australia",
+  "Germany",
+];
+const site = ["site:linkedin.com"];
+
+const buildQueries = () => {
+  //cache build
+  return professions
+    .map((profession) => {
+      return countries.map((country) => {
+        return emails.map((email) => {
+          return site.map((site) =>
+            encodeURIComponent(
+              `${site} ${profession} ${email} ${country}`
+            ).replace(/\%20/g, "+")
+          );
+        });
+      });
+    }, [])
+    .flat(Infinity);
+};
+
+//10 20 40 50 100
+const RESULTS_PER_PAGES = 100;
+// const QUERY = encodeURIComponent(
+('site:linkedin.com "software engineer" "@gmail.com" United States');
+
+const NO_FILTERS = 0;
+const GOOGLE_URL = `https://www.google.com/search?q=${QUERY}&filter=${NO_FILTERS}&num=${RESULTS_PER_PAGES}`;
 
 const { hashedEmails } = cachedData;
 
-const email = async (page) => {
-  await page.goto(GOOGLE_URL);
-  await page.type(SEARCH_INPUT_CLASS, SEARCH_INPUT, { delay: 25 });
-  await page.waitForSelector(SEARCH_BUTTON_CLASS);
-  await page.click(SEARCH_BUTTON_CLASS);
-  await page.waitForNavigation({ timeout: 1000 }).catch((err) => err);
+const email = async (
+  page,
+  goTo,
+  globalTimeout = 2000,
+  MAX_DATA = 500,
+  randomTimeout = true
+) => {
+  await page.goto(goTo);
+  await page.waitForNavigation({ timeout: globalTimeout }).catch((err) => err);
+  const _timeout = () =>
+    randomTimeout
+      ? (Math.floor(Math.random() * 5) + 1) * globalTimeout
+      : globalTimeout;
 
-  const locateEmails = async (resolve, emails, timeout = 1000) => {
-    setTimeout(async () => {
-      await page.waitForSelector(PAGE_RESULT_CLASS);
+  const locateEmails = async (emails, timeout = _timeout()) => {
+    await delay(timeout);
+    await page.waitForSelector(PAGE_RESULT_CLASS);
+    const _emailsFromPage = await page.evaluate(
+      extractEmailsAndNames,
+      PAGE_RESULT_CLASS
+    );
+    const emailsFromPage = await _emailsFromPage.reduce(
+      async (acc, emailObject) => {
+        const { email } = emailObject || {};
+        const _acc = await acc;
+        const emailHashed = hashData(email);
+        const duplicateEmail =
+          hashedEmails[emailHashed] ||
+          (await getEmail({ where: { emailHashed }, attributes: ["id"] }));
 
-      const _emailsFromPage = await page.evaluate(
-        extractEmailsAndNames,
-        PAGE_RESULT_CLASS
-      );
+        !duplicateEmail && _acc.push(emailObject);
+        hashedEmails[emailHashed] = true;
 
-      //finish ASYNC AWAIT
-      const [emailsFromPage] = await Promise.all(
-        _emailsFromPage.reduce(async ({ email }) => {
-          const duplicateEmail =
-            hashedEmails[hashData(email)] || (await getEmail(email));
-          return !duplicateEmail;
-        }, Promise.resolve([]))
-      );
+        return _acc;
+      },
+      []
+    );
 
-      emails.push(...emailsFromPage);
-      await page.click(NEXT_PAGE_ID);
-      await page.waitForNavigation({ timeout: 1000 }).catch((err) => err);
-      console.log(emails.length);
+    emails.push(...emailsFromPage);
+    await page.click(NEXT_PAGE_ID);
+    cachedData.PAGE += 1;
+    await page
+      .waitForNavigation({ timeout: globalTimeout })
+      .catch((err) => err);
 
-      const _timeout = !(emails.length % 100) ? 10000 : timeout;
-      if (emails.length < 12) return locateEmails(resolve, emails, _timeout);
-      return resolve(emails);
-    }, timeout);
+    console.log("PAGE:", cachedData.PAGE);
+    console.log("EMAILS FOUND IN THAT PAGE:", _emailsFromPage.length);
+    console.log(
+      "EMAILS FOUND IN THAT PAGE NOT DUPLICATED:",
+      emailsFromPage.length
+    );
+    console.log("TOTAL EMAILS SCRAPPED SO FAR:", emails.length);
+
+    cachedData.newEmails += emailsFromPage.length;
+    const stale = cachedData.newEmails / cachedData.PAGE;
+    console.log("Stale", stale);
+
+    if (stale < 0.52 && cachedData.PAGE > 20) return emails;
+    const _timeout =
+      emails.length % 100 === 0 && emails.length !== 0 ? 10000 : void 0;
+    if (emails.length < MAX_DATA) return locateEmails(emails);
+    return emails;
   };
 
   const startScrapper = () => {
     const emails = [];
-    return new Promise((resolve, rej) => {
-      return locateEmails(resolve, emails);
+    return locateEmails(emails).catch((e) => {
+      console.log("Error", e);
+      return emails;
     });
   };
-
-  const emails = await startScrapper();
-
-  console.log(emails);
+  return startScrapper();
 };
 
-export { email };
+const startQuery = async ({ browser, iterator, oldPage, data = [] }) => {
+  const queriesIterator = iterator || buildQueries()[Symbol.iterator]();
+  const { value: goTo, done } = queriesIterator.next();
+  const newPage = browser.newPage();
+
+  if (!done) return;
+
+  await delay(10000);
+  oldPage?.close();
+  data.concat(await email(newPage, goTo));
+
+  return startQuery({ oldPage: newPage, browser, iterator: queriesIterator });
+};
+
+export { email, startQuery };
--- puppeteer/index.js
+++ puppeteer/index.js
@@ -1,11 +1,14 @@
+import _fs from "fs";
 import _puppeteer from "puppeteer";
 import puppeteer from "puppeteer-extra";
-import { createEmail, extractEmailsAndNames } from "../controllers/email.js";
+import { createEmails } from "../controllers/email.js";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 import AdBlockerPlugin from "puppeteer-extra-plugin-adblocker";
 import AnonymizeUA from "puppeteer-extra-plugin-anonymize-ua";
 import RecaptchaPlugin from "puppeteer-extra-plugin-recaptcha";
-import { email } from "./email.js";
+import { email, startQuery } from "./email.js";
+import { cachedData } from "../cachedData.js";
+import { hashData } from "../utils.js";
 
 puppeteer.use(StealthPlugin());
 puppeteer.use(AdBlockerPlugin({ blockTrackers: true }));
@@ -13,6 +16,7 @@ puppeteer.use(AnonymizeUA());
 // puppeteer.use(RecaptchaPlugin());
 
 const { executablePath } = _puppeteer;
+const fs = _fs.promises;
 
 //
 const browser = await puppeteer.launch({
@@ -20,9 +24,29 @@ const browser = await puppeteer.launch({
   ignoreHTTPSErrors: true,
   executablePath: executablePath(),
 });
-const page = await browser.newPage();
-await email(page);
 
-// await Promise.all(emails.map(createEmail));
+const savedEmails = JSON.parse(
+  await fs.readFile("emails.list", "utf8").catch(() => "[]")
+);
 
-// await browser.close();
+savedEmails?.length &&
+  (await createEmails(savedEmails).catch((err) => {
+    console.log(err);
+    return [];
+  }));
+savedEmails?.forEach(
+  ({ email }) => (cachedData.hashedEmails[hashData(email)] = true)
+);
+
+const allEmails = await startQuery({ browser });
+console.log("allEmails", allEmails.length);
+const hasCreated = !!(
+  await createEmails(allEmails).catch((e) => {
+    console.log("Error bulking creating");
+    return [];
+  })
+).length;
+
+if (!hasCreated)
+  await fs.writeFile("emails.list", JSON.stringify(allEmails, null, 2));
+await browser.close();
