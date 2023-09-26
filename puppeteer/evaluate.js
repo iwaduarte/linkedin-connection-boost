7b0f7c900d96e7aaed40bd25f70133f39b081076
iwaduarte <iwaduarte@gmail.com>
2023-09-26 14:40:33 -0300

    feat: connect to users

---
+++ puppeteer/evaluate.js
@@ -0,0 +1,96 @@
+const evaluate = async () => {
+  const configuration = { interval: null };
+
+  const limitReached = () => {
+    const textLimit = document.querySelector(
+      "#ip-fuse-limit-alert__header"
+    )?.innerText;
+    return (
+      textLimit?.includes("reached the weekly invitation limit") ||
+      textLimit?.includes("limite semanal")
+    );
+  };
+  const findNextBatch = async () => {
+
+    const navigationBar = document
+      .querySelector(
+        "[aria-label=\"Next\"],[aria-label=\"Avançar\"], [aria-label=\"Show more results\"] "
+      );
+
+    if (navigationBar) {
+      scroll(0, document.body.clientHeight);
+      navigationBar.click();
+      return;
+    }
+
+    Array.from(
+      document.querySelectorAll(`.abi-saved-contacts__contact-list > li`)
+    ).forEach((e) => {
+      const element = e.querySelector(
+        '[aria-label="Connect"],[aria-label="Conectar"]'
+      );
+      if (element) return;
+      return e.remove();
+    });
+  };
+
+  const stopOperation = () => clearInterval(configuration.interval);
+  const interact = () => {
+    document.querySelector('[aria-label="Got it"],[aria-label="Okay"]')?.click();
+    const titleModal = document.getElementById("send-invite-modal")?.innerText;
+    if (
+      titleModal?.includes("How do you know") ||
+      titleModal?.includes("Como você conhece")
+    ) {
+      document
+        .querySelector('[aria-label="Other"],[aria-label="Outros"]')
+        ?.click();
+      document
+        .querySelector('[aria-label="Connect"],[aria-label="Conectar"]')
+        ?.click();
+    }
+    document
+      .querySelector('[aria-label="Send now"], [aria-label="Enviar agora"]')
+      ?.click();
+    document
+      .querySelector('[aria-label="Dismiss"],[aria-label="Cancelar"]')
+      ?.click();
+  };
+  const getAllConnections = () =>
+    Array.from(
+      document.querySelectorAll('[id*="ember"] > span.artdeco-button__text')
+    ).filter((e) => e.innerText === "Connect" || e.innerText === "Conectar");
+
+  const connect = async () => {
+    const allConnectionsIterator = getAllConnections()[Symbol.iterator]();
+    const stopConnect = await new Promise((resolve) => {
+      configuration.interval = setInterval(async () => {
+        const { value, done } = allConnectionsIterator.next();
+        console.log("Connection", value?.innerHTML);
+        if (done) {
+          stopOperation();
+          return resolve(false);
+        }
+        await new Promise((r) => setTimeout(r, 500));
+        value.click();
+        await new Promise((r) => setTimeout(r, 200));
+        if (limitReached()) {
+          stopOperation();
+          return resolve(true);
+        }
+        interact();
+      }, 1500);
+    });
+    console.log("Finished batch");
+    if (stopConnect) return;
+
+    await findNextBatch();
+    await new Promise((r) => setTimeout(r, 3500));
+    return connect();
+  };
+
+  await connect()
+
+}
+
+export { evaluate }
