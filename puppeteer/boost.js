162922c543c4f577a18f593990aed7fef433bd61
iwaduarte <iwaduarte@gmail.com>
2023-10-06 22:10:25 -0300

    refactor:  split boost to have a initialize/config

--- puppeteer/boost.js
+++ puppeteer/boost.js
@@ -1,66 +1,42 @@
-import dotenv from "dotenv";
-dotenv.config();
-import puppeteer from "puppeteer-extra";
-import StealthPlugin from "puppeteer-extra-plugin-stealth";
-import AnonymizeUA from "puppeteer-extra-plugin-anonymize-ua";
-import chromium from "@sparticuz/chromium";
 import { evaluate } from "./evaluate.js";
+import { botDetection, initBrowser, takeScreenshot } from "./utils.js";
 
-puppeteer.use(StealthPlugin());
-puppeteer.use(AnonymizeUA());
+const { USER_LOGIN, PASSWORD, IS_LOCAL_DEVELOPMENT, KEYWORDS } = process.env;
 
+const MAX_CONNECTIONS = 190;
+const ADD_REQUEST =
+  "https://www.linkedin.com/voyager/api/voyagerRelationshipsDashMemberRelationships";
 const LINKEDIN_URL = "https://linkedin.com";
-const { USER_LOGIN, PASSWORD, IS_LOCAL_DEVELOPMENT, LOCAL_PATH, KEYWORDS } =
-  process.env;
-
 const keywords = KEYWORDS
   ? encodeURIComponent(KEYWORDS.join(" "))
   : "cto hiring";
+const RESULTS_URL = "https://www.linkedin.com/search/results/people/";
 const SEARCH_URL = `https://www.linkedin.com/search/results/people/?keywords=${keywords}`;
-const ADD_REQUEST =
-  "https://www.linkedin.com/voyager/api/voyagerRelationshipsDashMemberRelationships";
-const MAX_CONNECTIONS = 102;
+const login = async (page) => {
+  await page.goto(LINKEDIN_URL);
+  const [error] = await page
+    .waitForSelector("#session_key", { timeout: 1500 })
+    .then((nodeElement) => [false, nodeElement])
+    .catch((err) => [err, false]);
 
-const opts =
-  IS_LOCAL_DEVELOPMENT === "true"
-    ? {
-        headless: false,
-        executablePath: LOCAL_PATH,
-        args: [
-          "--disable-web-security",
-          "--disable-features=IsolateOrigins,site-per-process",
-        ],
-      }
-    : {
-        headless: "new",
-        defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath(),
-        args: [
-          "--disable-web-security",
-          "--disable-features=IsolateOrigins,site-per-process",
-          // `--disable-extensions-except=${pathToExtension}`,
-          // `--load-extension=${pathToExtension}`,
-          ...chromium.args,
-        ],
-      };
-
-await fetch("https://checkip.amazonaws.com/")
-  .then((response) => response.text())
-  .then((data) => {
-    console.log(`Your public IP address is: ${data.trim()}`);
-  })
-  .catch((error) => {
-    console.log("An error occurred:", error);
-  });
-
+  if (error) {
+    console.log("Skipping login, already connected");
+    return;
+  }
+  await page.type("#session_key", USER_LOGIN, { delay: 100 });
+  await page.type("#session_password", PASSWORD, { delay: 100 });
+  await page.click('button[type="submit"]');
+  await takeScreenshot(page);
+  await new Promise((r) => setTimeout(r, 2000));
+};
 const addContacts = async () => {
-  const browser = await puppeteer.launch({
-    ignoreHTTPSErrors: true,
-    ...(IS_LOCAL_DEVELOPMENT === "true" ? { userDataDir: "./user-data" } : {}),
-    ...opts,
-  });
-
-  const [page] = await browser.pages();
+  const browser = await initBrowser();
+  // browser.pages() doesn't work for avoid bot detection
+  // See: https://github.com/puppeteer/puppeteer/issues/2669
+  // Full reference: https://github.com/berstend/puppeteer-extra/blob/39248f1f5deeb21b1e7eb6ae07b8ef73f1231ab9/packages/puppeteer-extra/src/index.ts#L234
+  const page = await browser.newPage();
+  await page.setBypassCSP(true);
+  await botDetection(page);
 
   let postRequestCount = 0;
   page.on("request", async (request) => {
@@ -79,26 +55,14 @@ const addContacts = async () => {
     }
   });
 
-  await page.goto(
-    "https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html",
-    { waitUntil: "networkidle2" }
-  );
-  const screenshot = await page.screenshot({ encoding: "base64" });
-  console.log(" screenshot", screenshot);
+  await login(page);
+  await page.goto(SEARCH_URL, { waitUntil: "networkidle2" });
+  await page.waitForNavigation({ timeout: 1000 }).catch((err) => err);
 
-  if (await page.$(".join-form__form-body-submit-button")) {
-    await page.goto(LINKEDIN_URL);
-    await page
-      .waitForSelector("#session_key", { timeout: 2000 })
-      .catch((err) => err);
-    await page.type("#session_key", USER_LOGIN, { delay: 100 });
-    await page.type("#session_password", PASSWORD, { delay: 100 });
-    await page.click('button[type="submit"]');
-    await new Promise((r) => setTimeout(r, 2000));
-    await page.goto(SEARCH_URL, { waitUntil: "networkidle2" });
-    await page.waitForNavigation({ timeout: 1000 }).catch((err) => err);
+  if (!page.url().includes(RESULTS_URL)) {
+    await takeScreenshot(page);
+    return browser.close();
   }
-  if (!page.url().includes(SEARCH_URL)) return;
 
   await page.evaluate(evaluate);
   await browser.close();
